add9642b2fdb880f3e4988c4ea3b5c1d10122fa6
Keith Robertson <robertsonk91@gmail.com>
2024-07-13 16:35:45 +0100

    Add sonarqube scanning (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("jacoco")
+    id("org.sonarqube") version "5.1.0.4882"
 }
 
 group = "com.keith"
@@ -23,6 +24,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.hamcrest:hamcrest:2.2")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "KeithRobertson_Kata")
+        property("sonar.host.url", "https://sonarcloud.io/")
+    }
 }
 
 tasks.test {
